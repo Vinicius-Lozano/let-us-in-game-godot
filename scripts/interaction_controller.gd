2284d64da83b025e717c485779f9e17fe1340b5f
vinicius-lozano <viniciuslozanodeoliveia@gmail.com>
2026-04-04 19:42:13 -0300

    feat(player): add InteractionController to grab items with mouse
    Introduce the InteractionController component to the player.
    This allows the player to click on interactable items using
    the left mouse button and carry them around the map, making
    the item follow the mouse cursor.

---
+++ scripts/interaction_controller.gd
@@ -0,0 +1,38 @@
+extends Node
+
+@onready var interaction_controller: Node = %InteractionController
+@onready var interaction_raycast: RayCast3D = $"../Head/Eyes/Camera3D/InteractionRaycast"
+@onready var player_camera: Camera3D = $"../Head/Eyes/Camera3D"
+@onready var hand: Marker3D = %Hand
+
+var current_object: Object
+var last_potential_object: Object
+var interaction_component: Node
+
+func _process(delta: float) -> void:
+	if current_object:
+		if Input.is_action_just_pressed("hand_secondary"):
+			if interaction_component:
+				interaction_component.auxInteract()
+				current_object = null
+		elif Input.is_action_pressed("hand_primary"):
+			if interaction_component:
+				interaction_component.interact()
+		else: 
+			if interaction_component:
+				interaction_component.postInteract()
+				current_object = null
+	else:
+		var potential_object: Object = interaction_raycast.get_collider()
+		
+		if potential_object and potential_object is Node:
+			interaction_component = potential_object.get_node_or_null("InteractionComponent")
+			if interaction_component:
+				if interaction_component.can_interact == false:
+					return
+			
+				last_potential_object = current_object
+			
+				if Input.is_action_pressed("hand_primary"):
+					current_object = potential_object
+					interaction_component.preInteract(hand)
